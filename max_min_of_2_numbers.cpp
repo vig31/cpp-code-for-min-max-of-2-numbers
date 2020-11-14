9cd2482e5835120f6e0ce9ac8b1ca84536bab97a
vig31 <40334082+vig31@users.noreply.github.com>
2020-11-14 08:19:42 +0530

    Add files via upload

---
+++ max_min_of_2_numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a,b;
+    cout<< "enter 2 numbers to find max and min" << endl;
+    cin>> a,b;
+    if (a>b)
+    {
+        cout<< "max is a"; 
+        cout<< "min is" ;
+    }
+    else
+    {
+        cout<<"max is" << endl;  
+        cout<< "min is"; 
+    }
+    return 0 ;
+}
